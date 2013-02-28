087c814e11c55d4879d727b84f896fe70de20f2f
PilzAdam <PilzAdam@gmx.de>
2013-02-28 16:59:01 +0100

    Fix texture names, add crafting for power- and brakerail and make them usable without mesecons

--- init.lua
+++ init.lua
@@ -462,9 +462,9 @@ minetest.register_node(":default:rail", {
 minetest.register_node("carts:powerrail", {
 	description = "Powered Rail",
 	drawtype = "raillike",
-	tiles = {"default_rail_pwr.png", "default_rail_curved_pwr.png", "default_rail_t_junction_pwr.png", "default_rail_crossing_pwr.png"},
-	inventory_image = "default_rail_pwr.png",
-	wield_image = "default_rail_pwr.png",
+	tiles = {"carts_rail_pwr.png", "carts_rail_curved_pwr.png", "carts_rail_t_junction_pwr.png", "carts_rail_crossing_pwr.png"},
+	inventory_image = "carts_rail_pwr.png",
+	wield_image = "carts_rail_pwr.png",
 	paramtype = "light",
 	is_ground_content = true,
 	walkable = false,
@@ -474,6 +474,12 @@ minetest.register_node("carts:powerrail", {
 		fixed = {-1/2, -1/2, -1/2, 1/2, -1/2+1/16, 1/2},
 	},
 	groups = {bendy=2,snappy=1,dig_immediate=2,attached_node=1,rail=1},
+	
+	after_place_node = function(pos, placer, itemstack)
+		if not mesecon then
+			minetest.env:get_meta(pos):set_string("cart_acceleration", "0.5")
+		end
+	end,
 	
 	mesecons = {
 		effector = {
@@ -491,9 +497,9 @@ minetest.register_node("carts:powerrail", {
 minetest.register_node("carts:brakerail", {
 	description = "Brake Rail",
 	drawtype = "raillike",
-	tiles = {"default_rail_brk.png", "default_rail_curved_brk.png", "default_rail_t_junction_brk.png", "default_rail_crossing_brk.png"},
-	inventory_image = "default_rail_brk.png",
-	wield_image = "default_rail_brk.png",
+	tiles = {"carts_rail_brk.png", "carts_rail_curved_brk.png", "carts_rail_t_junction_brk.png", "carts_rail_crossing_brk.png"},
+	inventory_image = "carts_rail_brk.png",
+	wield_image = "carts_rail_brk.png",
 	paramtype = "light",
 	is_ground_content = true,
 	walkable = false,
@@ -503,6 +509,12 @@ minetest.register_node("carts:brakerail", {
 		fixed = {-1/2, -1/2, -1/2, 1/2, -1/2+1/16, 1/2},
 	},
 	groups = {bendy=2,snappy=1,dig_immediate=2,attached_node=1,rail=1},
+	
+	after_place_node = function(pos, placer, itemstack)
+		if not mesecon then
+			minetest.env:get_meta(pos):set_string("cart_acceleration", "-0.2")
+		end
+	end,
 	
 	mesecons = {
 		effector = {
@@ -515,4 +527,40 @@ minetest.register_node("carts:brakerail", {
 			end,
 		},
 	},
+})
+
+minetest.register_craft({
+	output = "carts:powerrail 2",
+	recipe = {
+		{"default:steel_ingot", "default:mese_crystal_fragment", "default:steel_ingot"},
+		{"default:steel_ingot", "default:stick", "default:steel_ingot"},
+		{"default:steel_ingot", "", "default:steel_ingot"},
+	}
+})
+
+minetest.register_craft({
+	output = "carts:powerrail 2",
+	recipe = {
+		{"default:steel_ingot", "", "default:steel_ingot"},
+		{"default:steel_ingot", "default:stick", "default:steel_ingot"},
+		{"default:steel_ingot", "default:mese_crystal_fragment", "default:steel_ingot"},
+	}
+})
+
+minetest.register_craft({
+	output = "carts:brakerail 2",
+	recipe = {
+		{"default:steel_ingot", "default:coal_lump", "default:steel_ingot"},
+		{"default:steel_ingot", "default:stick", "default:steel_ingot"},
+		{"default:steel_ingot", "", "default:steel_ingot"},
+	}
+})
+
+minetest.register_craft({
+	output = "carts:brakerail 2",
+	recipe = {
+		{"default:steel_ingot", "", "default:steel_ingot"},
+		{"default:steel_ingot", "default:stick", "default:steel_ingot"},
+		{"default:steel_ingot", "default:coal_lump", "default:steel_ingot"},
+	}
 })
